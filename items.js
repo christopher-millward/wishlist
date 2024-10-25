2b4e27db48f9dacd5a74843f5e599184bc92dbdc
cmillward13 <chris.millward13@hotmail.com>
2024-10-25 15:38:38 -0400

    Initial commit

---
+++ items.js
@@ -0,0 +1,129 @@
+const items = [{
+    "id": 1,
+    "name":"Klein Bottle (big)",
+    "price":"73",
+    "category":"Trinkets",
+    "image":"./images/klein-bottle.jpg",
+    "url": "https://www.kleinbottle.com/bigclassicalklein.htm"
+},
+{
+    "id": 2,
+    "name":"Pythagoras Cup",
+    "price":"25",
+    "category":"Trinkets",
+    "image":"./images/pythagoras-cup.jpg",
+    "url": "https://www.etsy.com/ca/listing/1165278110/pythagorean-cup"
+},
+{
+    "id": 3,
+    "name":"Fermentation kit",
+    "price":"50",
+    "category":"Hobbies",
+    "image":"./images/fermentation-kit.jpg",
+    "url": "https://www.amazon.ca/Home-Brew-Ohio-Fermenter-Capacity/dp/B00KQN9OSK/"
+},
+{
+    "id": 4,
+    "name":"Distillation kit (13 pc)",
+    "price":"87",
+    "category":"Hobbies",
+    "image":"./images/distilation-kit.jpg",
+    "url": "https://www.vevor.ca/lab-distillation-kit-c_11053/vevor-glass-organic-chemistry-kit-distillation-kit-13pcs-24-40-lab-glassware-kit-p_010785941292"
+},
+{
+    "id": 5,
+    "name":"Hues and Clues",
+    "price":"35",
+    "category":"Games",
+    "image":"./images/hues-and-clues.jpg",
+    "url": "https://www.calendarclub.ca/products/hues-and-cues-prd202208796"
+},
+{
+    "id": 6,
+    "name":"The Algorithm Design Manual (Hardcover)",
+    "price":"98",
+    "category":"Books",
+    "image":"./images/the-algorithm-design-manual.jpg",
+    "url": "https://www.amazon.ca/Algorithm-Design-Manual-Steven-Skiena/dp/3030542556/"
+},
+{
+    "id": 7,
+    "name":"Kodak's Ergonomic Design for People at Work",
+    "price":"242",
+    "category":"Books",
+    "image":"./images/kodak.jpg",
+    "url": "https://www.amazon.ca/Kodaks-Ergonomic-Design-People-Work/dp/0471418633"
+},
+{
+    "id": 8,
+    "name":"AMA Guide to the Evaluation of Disease and Injury Causation",
+    "price":"78",
+    "category":"Books",
+    "image":"./images/ama.jpeg",
+    "url": "https://www.thriftbooks.com/w/ama-guides-to-the-evaluation-of-disease-and-injury-causation/18670494/#edition=19894439&idiq=35928627"
+},
+{
+    "id": 9,
+    "name":"Thinking Fast and Slow",
+    "price":"23",
+    "category":"Books",
+    "image":"./images/thinking-fast-and-slow.jpg",
+    "url": "https://www.amazon.ca/Thinking-Fast-Slow-Daniel-Kahneman/dp/0385676530/"
+},
+{
+    "id": 10,
+    "name":"Pasta Roller",
+    "price":"102",
+    "category":"Kitchen",
+    "image":"./images/pasta-roller.jpg",
+    "url": "https://www.amazon.ca/Marcato-Atlas-Wellness-Pasta-Stainless/dp/B0009U5OSO/"
+},
+{
+    "id": 11,
+    "name":"Converse (size 8.5)",
+    "price":"75",
+    "category":"Fashion",
+    "image":"./images/converse.jpg",
+    "url": "https://www.sportchek.ca/en/pdp/converse-men-s-chuck-taylor-all-star-ox-low-top-casual-shoes-sneakers-70272176f.331505160.html"
+},
+{
+    "id": 12,
+    "name":"Wireless Keyboard",
+    "price":"33",
+    "category":"Tech",
+    "image":"./images/keyboard.jpg",
+    "url": "https://www.walmart.ca/en/ip/Three-fold-Bluetooth-Keyboard-Bluetooth-Portable-Mini-Wireless-Keyboard-with-Touchpad-Mouse-for-Android-Windows-PC-Tablet/1JXN4X5I36WB?"
+},
+{
+    "id": 13,
+    "name":"Laptop Stand",
+    "price":"33",
+    "category":"Tech",
+    "image":"./images/laptop-stand.jpg",
+    "url": "https://www.amazon.ca/OMOTON-Ergonomic-Adjustable-Compatible-iPad%EF%BC%8810-15-6/dp/B0B6V32P8V/ref=sr_1_4?th=1"
+},
+{
+    "id": 14,
+    "name":"Designing for People: An Introduction to Human Factors Engineering",
+    "price":"70",
+    "category":"Books",
+    "image":"./images/design-for-people.jpg",
+    "url": "https://www.amazon.com/Designing-People-Introduction-Factors-Engineering/dp/1539808009"
+},
+{
+    "id": 15,
+    "name":"Spotify Gift Card",
+    "price":"30",
+    "category":"Gift Card",
+    "image":"./images/spotify.jpg",
+    "url": "https://www.walmart.ca/en/ip/Spotify-30-Gift-Card-Digital-Code/6000205858300"
+},
+{
+    "id": 16,
+    "name":"Uber Gift Card",
+    "price":"25",
+    "category":"Gift Card",
+    "image":"./images/uber.jpg",
+    "url": "https://www.walmart.ca/en/ip/uber-cobranded-25-egift-card-email-delivery/6000206061197"
+}
+]
