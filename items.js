af52b0abc15f70af617b9a76d4fedda18f010c3a
cmillward13 <chris.millward13@hotmail.com>
2025-12-02 23:59:04 -0500

    updated Wishlist

--- items.js
+++ items.js
@@ -30,48 +30,8 @@ const items = [{
     "image":"./images/distilation-kit.jpg",
     "url": "https://www.vevor.ca/lab-distillation-kit-c_11053/vevor-glass-organic-chemistry-kit-distillation-kit-13pcs-24-40-lab-glassware-kit-p_010785941292"
 },
-// {
-//     "id": 5,
-//     "name":"Hues and Clues",
-//     "price":"35",
-//     "category":"Misc",
-//     "image":"./images/hues-and-clues.jpg",
-//     "url": "https://www.calendarclub.ca/products/hues-and-cues-prd202208796"
-// },
 {
-    "id": 6,
-    "name":"The Algorithm Design Manual (Hardcover)",
-    "price":"98",
-    "category":"Books",
-    "image":"./images/the-algorithm-design-manual.jpg",
-    "url": "https://www.amazon.ca/Algorithm-Design-Manual-Steven-Skiena/dp/3030542556/"
-},
-{
-    "id": 8,
-    "name":"AMA Guide to the Evaluation of Disease and Injury Causation",
-    "price":"78",
-    "category":"Books",
-    "image":"./images/ama.jpeg",
-    "url": "https://www.thriftbooks.com/w/ama-guides-to-the-evaluation-of-disease-and-injury-causation/18670494/#edition=19894439&idiq=35928627"
-},
-// {
-//     "id": 9,
-//     "name":"Thinking Fast and Slow",
-//     "price":"23",
-//     "category":"Books",
-//     "image":"./images/thinking-fast-and-slow.jpg",
-//     "url": "https://www.amazon.ca/Thinking-Fast-Slow-Daniel-Kahneman/dp/0385676530/"
-// },
-// {
-//     "id": 10,
-//     "name":"Pasta Roller",
-//     "price":"102",
-//     "category":"Kitchen",
-//     "image":"./images/pasta-roller.jpg",
-//     "url": "https://www.amazon.ca/Marcato-Atlas-Wellness-Pasta-Stainless/dp/B0009U5OSO/"
-// },
-{
-    "id": 11,
+    "id": 5,
     "name":"Converse (size 8.5)",
     "price":"75",
     "category":"Misc",
@@ -79,43 +39,59 @@ const items = [{
     "url": "https://www.sportchek.ca/en/pdp/converse-men-s-chuck-taylor-all-star-ox-low-top-casual-shoes-sneakers-70272176f.331505160.html"
 },
 {
-    "id": 12,
+    "id": 6,
     "name":"Wireless Keyboard",
     "price":"33",
     "category":"Tech",
     "image":"./images/keyboard.jpg",
     "url": "https://www.walmart.ca/en/ip/Three-fold-Bluetooth-Keyboard-Bluetooth-Portable-Mini-Wireless-Keyboard-with-Touchpad-Mouse-for-Android-Windows-PC-Tablet/1JXN4X5I36WB?"
 },
-// {
-//     "id": 13,
-//     "name":"Laptop Stand",
-//     "price":"15",
-//     "category":"Tech",
-//     "image":"./images/laptop-stand.jpg",
-//     "url": "https://www.amazon.ca/OMOTON-Ergonomic-Adjustable-Compatible-iPad%EF%BC%8810-15-6/dp/B0B6V32P8V/ref=sr_1_4?th=1"
-// },
 {
-    "id": 14,
-    "name":"Designing for People: An Introduction to Human Factors Engineering",
-    "price":"70",
-    "category":"Books",
-    "image":"./images/design-for-people.jpg",
-    "url": "https://www.amazon.com/Designing-People-Introduction-Factors-Engineering/dp/1539808009"
-},
-{
-    "id": 15,
-    "name":"Weighted Blanket",
-    "price":"35",
-    "category":"Misc",
-    "image":"./images/weighted-blanket.jpg",
-    "url": "https://en.sleepcountry.ca/products/blanquil-weighted-blanket.html"
-},
-{
-    "id": 16,
+    "id": 7,
     "name":"Meat Grinder",
     "price":"40",
     "category":"Kitchen",
     "image":"./images/meat-grinder.jpg",
     "url": "https://www.amazon.ca/Grinder-Stainless-Powerful-Effortless-Vegetables/dp/B07MWZ6NCV/"
-}
+},
+{
+    "id": 8,
+    "name":"Piping Bags",
+    "price":"10",
+    "category":"Kitchen",
+    "image":"./images/piping-bags.jpg",
+    "url": "https://www.amazon.ca/Decorating-Supplies-Scraper-Couplers-Convenient/dp/B0C6SSH4LK/?th=1"
+},
+{
+    "id": 9,
+    "name":"Brulee Torch",
+    "price":"22",
+    "category":"Kitchen",
+    "image":"./images/brulee-torch.jpg",
+    "url": "https://www.amazon.ca/Sondiko-Culinary-Refillable-Adjustable-Desserts/dp/B07TVHHYFG?th=1"
+},
+{
+    "id": 10,
+    "name":"Food Thermometer",
+    "price":"16",
+    "category":"Kitchen",
+    "image":"./images/food-thermometer.jpg",
+    "url": "https://www.amazon.ca/Alpha-Grillers-Meat-Thermometer-Digital/dp/B0F6F4W5JW/?th=1"
+},
+{
+    "id": 11,
+    "name":"Fine Mesh Chinois",
+    "price":"100",
+    "category":"Kitchen",
+    "image":"./images/food-thermometer.jpg",
+    "url": "https://www.amazon.ca/Premium-Stainless-Reinforced-Chinois-Strainer/dp/B0CVLDXBBY/"
+},
+{
+    "id": 12,
+    "name":"Cat's Cradle",
+    "price":"23",
+    "category":"Kitchen",
+    "image":"./images/cats-cradle.jpg",
+    "url": "https://www.amazon.ca/Cats-Cradle-Novel-Kurt-Vonnegut/dp/038533348X"
+},
 ]
