dbcfb34be375345c316d1d9f6909cfe64913061c
cmillward13 <chris.millward13@hotmail.com>
2025-12-27 14:37:05 -0500

    -Updated wishlist. -Refactored to remove item IDs. IDs were never accessed and were annoying to manually update each time items were added / removed. Will just add an auto-incrementing id function to the script if I need IDs int he future.

--- items.js
+++ items.js
@@ -1,5 +1,4 @@
 const items = [{
-    "id": 1,
     "name":"Klein Bottle (big)",
     "price":"73",
     "category":"Nerd stuff",
@@ -7,15 +6,6 @@ const items = [{
     "url": "https://www.kleinbottle.com/bigclassicalklein.htm"
 },
 {
-    "id": 2,
-    "name":"Pythagoras Cup",
-    "price":"25",
-    "category":"Nerd stuff",
-    "image":"./images/pythagoras-cup.jpg",
-    "url": "https://www.etsy.com/ca/listing/1165278110/pythagorean-cup"
-},
-{
-    "id": 3,
     "name":"Fermentation kit",
     "price":"50",
     "category":"Hobbies",
@@ -23,7 +13,6 @@ const items = [{
     "url": "https://www.amazon.ca/Home-Brew-Ohio-Fermenter-Capacity/dp/B00KQN9OSK/"
 },
 {
-    "id": 4,
     "name":"Distillation kit (13 pc)",
     "price":"87",
     "category":"Hobbies",
@@ -31,7 +20,6 @@ const items = [{
     "url": "https://www.vevor.ca/lab-distillation-kit-c_11053/vevor-glass-organic-chemistry-kit-distillation-kit-13pcs-24-40-lab-glassware-kit-p_010785941292"
 },
 {
-    "id": 5,
     "name":"Converse (size 8.5)",
     "price":"75",
     "category":"Misc",
@@ -39,23 +27,6 @@ const items = [{
     "url": "https://www.sportchek.ca/en/pdp/converse-men-s-chuck-taylor-all-star-ox-low-top-casual-shoes-sneakers-70272176f.331505160.html"
 },
 {
-    "id": 6,
-    "name":"Wireless Keyboard",
-    "price":"33",
-    "category":"Tech",
-    "image":"./images/keyboard.jpg",
-    "url": "https://www.walmart.ca/en/ip/Three-fold-Bluetooth-Keyboard-Bluetooth-Portable-Mini-Wireless-Keyboard-with-Touchpad-Mouse-for-Android-Windows-PC-Tablet/1JXN4X5I36WB?"
-},
-{
-    "id": 7,
-    "name":"Meat Grinder",
-    "price":"40",
-    "category":"Kitchen",
-    "image":"./images/meat-grinder.jpg",
-    "url": "https://www.amazon.ca/Grinder-Stainless-Powerful-Effortless-Vegetables/dp/B07MWZ6NCV/"
-},
-{
-    "id": 8,
     "name":"Piping Bags",
     "price":"10",
     "category":"Kitchen",
@@ -63,15 +34,6 @@ const items = [{
     "url": "https://www.amazon.ca/Decorating-Supplies-Scraper-Couplers-Convenient/dp/B0C6SSH4LK/?th=1"
 },
 {
-    "id": 9,
-    "name":"Brulee Torch",
-    "price":"22",
-    "category":"Kitchen",
-    "image":"./images/brulee-torch.jpg",
-    "url": "https://www.amazon.ca/Sondiko-Culinary-Refillable-Adjustable-Desserts/dp/B07TVHHYFG?th=1"
-},
-{
-    "id": 10,
     "name":"Food Thermometer",
     "price":"16",
     "category":"Kitchen",
@@ -79,7 +41,6 @@ const items = [{
     "url": "https://www.amazon.ca/Alpha-Grillers-Meat-Thermometer-Digital/dp/B0F6F4W5JW/?th=1"
 },
 {
-    "id": 11,
     "name":"Fine Mesh Chinois",
     "price":"100",
     "category":"Kitchen",
@@ -87,10 +48,9 @@ const items = [{
     "url": "https://www.amazon.ca/Premium-Stainless-Reinforced-Chinois-Strainer/dp/B0CVLDXBBY/"
 },
 {
-    "id": 12,
     "name":"Cat's Cradle",
     "price":"23",
-    "category":"Kitchen",
+    "category":"Misc",
     "image":"./images/cats-cradle.jpg",
     "url": "https://www.amazon.ca/Cats-Cradle-Novel-Kurt-Vonnegut/dp/038533348X"
 },
